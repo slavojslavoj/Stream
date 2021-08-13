35a16efb6a1ee607546ab6dab616d2d69a73f25a
slavoj <slavojslavoj@icloud.com>
2021-08-13 11:55:28 +0300

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,10 +10,25 @@ public class Main {
         for (int i = 0; i <100; i++) {
             numbers.add((int)(Math.random() * 1000));
         }
-        List<Integer> filtered = filter(numbers, (n) -> n % 5 ==0);
-        for (int i : filtered) {
-            System.out.println(i);
+
+        List<String> list = numbers.stream()
+                .filter((n) -> n % 2 ==0)
+                .map((integer -> "Number: " + integer))
+                .filter((string) -> string.endsWith("0"))
+                .map((string) -> string + "!")
+                .collect(Collectors.toList());
+
+        for (String s : list) {
+            System.out.println(s);
         }
+    }
+
+    private static  List<String> map(List<Integer> numbers) {
+        List<String> result = new ArrayList<>();
+        for (int number : numbers) {
+            result.add("Number: " + number);
+        }
+        return result;
     }
 
     private static List<Integer> filter(List<Integer> list, Predicate predicate) {
