3a5cc0f9cca331d14b02e0b7febc3b1943fb156c
Дмитрий Петроченков <slavoj@MacBook-Pro-Dmitrij.local>
2021-08-13 10:56:11 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i <100; i++) {
+            numbers.add((int)(Math.random() * 1000));
+        }
+        List<Integer> filtered = filter(numbers, (n) -> n % 5 ==0);
+        for (int i : filtered) {
+            System.out.println(i);
+        }
+    }
+
+    private static List<Integer> filter(List<Integer> list, Predicate predicate) {
+        List<Integer> result = new ArrayList<>();
+        for (int i : list) {
+            if (predicate.test(i)) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
